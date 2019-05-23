9fe984eba847daac5d6db697e5f07d6b30288969
Oscar Barajas <oscar@dragons.mx>
2019-05-23 18:16:41 -0500

    Initial commit from gatsby: (https://github.com/TryGhost/gatsby-starter-ghost.git)

---
+++ gatsby-node.js
@@ -0,0 +1,273 @@
+const path = require(`path`)
+const config = require(`./src/utils/siteConfig`)
+const { paginate } = require(`gatsby-awesome-pagination`)
+
+/**
+* Here is the place where Gatsby creates the URLs for all the
+* posts, tags, pages and authors that we fetched from the Ghost site.
+*/
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+
+    /**
+    * Posts
+    */
+    const createPosts = new Promise((resolve, reject) => {
+        const postTemplate = path.resolve(`./src/templates/post.js`)
+        const indexTemplate = path.resolve(`./src/templates/index.js`)
+        resolve(
+            graphql(`
+                {
+                    allGhostPost(
+                        sort: {order: ASC, fields: published_at}
+                    ) {
+                        edges {
+                            node {
+                                slug
+                            }
+                        }
+                    }
+                }`
+            ).then((result) => {
+                if (result.errors) {
+                    return reject(result.errors)
+                }
+
+                if (!result.data.allGhostPost) {
+                    return resolve()
+                }
+
+                const items = result.data.allGhostPost.edges
+
+                items.forEach(({ node }) => {
+                    // This part here defines, that our posts will use
+                    // a `/:slug/` permalink.
+                    node.url = `/${node.slug}/`
+
+                    createPage({
+                        path: node.url,
+                        component: path.resolve(postTemplate),
+                        context: {
+                            // Data passed to context is available
+                            // in page queries as GraphQL variables.
+                            slug: node.slug,
+                        },
+                    })
+                })
+
+                // Pagination for posts, e.g., /, /page/2, /page/3
+                paginate({
+                    createPage,
+                    items: items,
+                    itemsPerPage: config.postsPerPage,
+                    component: indexTemplate,
+                    pathPrefix: ({ pageNumber }) => {
+                        if (pageNumber === 0) {
+                            return `/`
+                        } else {
+                            return `/page`
+                        }
+                    },
+                })
+
+                return resolve()
+            })
+        )
+    })
+
+    /**
+    * Tags
+    */
+    const createTags = new Promise((resolve, reject) => {
+        const tagsTemplate = path.resolve(`./src/templates/tag.js`)
+        resolve(
+            graphql(`
+                {
+                    allGhostTag(
+                        sort: {order: ASC, fields: name}
+                    ) {
+                        edges {
+                            node {
+                                slug
+                                url
+                                postCount
+                            }
+                        }
+                    }
+                }`
+            ).then((result) => {
+                if (result.errors) {
+                    return reject(result.errors)
+                }
+
+                if (!result.data.allGhostTag) {
+                    return resolve()
+                }
+
+                const items = result.data.allGhostTag.edges
+                const postsPerPage = config.postsPerPage
+
+                items.forEach(({ node }) => {
+                    const totalPosts = node.postCount !== null ? node.postCount : 0
+                    const numberOfPages = Math.ceil(totalPosts / postsPerPage)
+
+                    // This part here defines, that our tag pages will use
+                    // a `/tag/:slug/` permalink.
+                    node.url = `/tag/${node.slug}/`
+
+                    Array.from({ length: numberOfPages }).forEach((_, i) => {
+                        const currentPage = i + 1
+                        const prevPageNumber = currentPage <= 1 ? null : currentPage - 1
+                        const nextPageNumber = currentPage + 1 > numberOfPages ? null : currentPage + 1
+                        const previousPagePath = prevPageNumber ? prevPageNumber === 1 ? node.url : `${node.url}page/${prevPageNumber}/` : null
+                        const nextPagePath = nextPageNumber ? `${node.url}page/${nextPageNumber}/` : null
+
+                        createPage({
+                            path: i === 0 ? node.url : `${node.url}page/${i + 1}/`,
+                            component: path.resolve(tagsTemplate),
+                            context: {
+                                // Data passed to context is available
+                                // in page queries as GraphQL variables.
+                                slug: node.slug,
+                                limit: postsPerPage,
+                                skip: i * postsPerPage,
+                                numberOfPages: numberOfPages,
+                                humanPageNumber: currentPage,
+                                prevPageNumber: prevPageNumber,
+                                nextPageNumber: nextPageNumber,
+                                previousPagePath: previousPagePath,
+                                nextPagePath: nextPagePath,
+                            },
+                        })
+                    })
+                })
+
+                return resolve()
+            })
+        )
+    })
+
+    /**
+    * Authors
+    */
+    const createAuthors = new Promise((resolve, reject) => {
+        const authorTemplate = path.resolve(`./src/templates/author.js`)
+        resolve(
+            graphql(`
+                {
+                    allGhostAuthor(
+                        sort: {order: ASC, fields: name}
+                    ) {
+                        edges {
+                            node {
+                                slug
+                                url
+                                postCount
+                            }
+                        }
+                    }
+                }`
+            ).then((result) => {
+                if (result.errors) {
+                    return reject(result.errors)
+                }
+
+                if (!result.data.allGhostAuthor) {
+                    return resolve()
+                }
+
+                const items = result.data.allGhostAuthor.edges
+                const postsPerPage = config.postsPerPage
+
+                items.forEach(({ node }) => {
+                    const totalPosts = node.postCount !== null ? node.postCount : 0
+                    const numberOfPages = Math.ceil(totalPosts / postsPerPage)
+
+                    // This part here defines, that our author pages will use
+                    // a `/author/:slug/` permalink.
+                    node.url = `/author/${node.slug}/`
+
+                    Array.from({ length: numberOfPages }).forEach((_, i) => {
+                        const currentPage = i + 1
+                        const prevPageNumber = currentPage <= 1 ? null : currentPage - 1
+                        const nextPageNumber = currentPage + 1 > numberOfPages ? null : currentPage + 1
+                        const previousPagePath = prevPageNumber ? prevPageNumber === 1 ? node.url : `${node.url}page/${prevPageNumber}/` : null
+                        const nextPagePath = nextPageNumber ? `${node.url}page/${nextPageNumber}/` : null
+
+                        createPage({
+                            path: i === 0 ? node.url : `${node.url}page/${i + 1}/`,
+                            component: path.resolve(authorTemplate),
+                            context: {
+                                // Data passed to context is available
+                                // in page queries as GraphQL variables.
+                                slug: node.slug,
+                                limit: postsPerPage,
+                                skip: i * postsPerPage,
+                                numberOfPages: numberOfPages,
+                                humanPageNumber: currentPage,
+                                prevPageNumber: prevPageNumber,
+                                nextPageNumber: nextPageNumber,
+                                previousPagePath: previousPagePath,
+                                nextPagePath: nextPagePath,
+                            },
+                        })
+                    })
+                })
+                return resolve()
+            })
+        )
+    })
+
+    /**
+    * Pages
+    */
+    const createPages = new Promise((resolve, reject) => {
+        const pageTemplate = path.resolve(`./src/templates/page.js`)
+        resolve(
+            graphql(`
+                {
+                    allGhostPage(
+                        sort: {order: ASC, fields: published_at}
+                    ) {
+                        edges {
+                            node {
+                                slug
+                                url
+                            }
+                        }
+                    }
+                }`
+            ).then((result) => {
+                if (result.errors) {
+                    return reject(result.errors)
+                }
+
+                if (!result.data.allGhostPage) {
+                    return resolve()
+                }
+
+                const items = result.data.allGhostPage.edges
+
+                items.forEach(({ node }) => {
+                    // This part here defines, that our pages will use
+                    // a `/:slug/` permalink.
+                    node.url = `/${node.slug}/`
+
+                    createPage({
+                        path: node.url,
+                        component: path.resolve(pageTemplate),
+                        context: {
+                            // Data passed to context is available
+                            // in page queries as GraphQL variables.
+                            slug: node.slug,
+                        },
+                    })
+                })
+
+                return resolve()
+            })
+        )
+    })
+
+    return Promise.all([createPosts, createTags, createAuthors, createPages])
+}
---
+++ src/components/common/meta/AuthorMeta.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import PropTypes from 'prop-types'
+import _ from 'lodash'
+import { StaticQuery, graphql } from 'gatsby'
+
+import ImageMeta from './ImageMeta'
+import getAuthorProperties from './getAuthorProperties'
+import config from '../../../utils/siteConfig'
+
+const AuthorMeta = ({ data, settings, canonical }) => {
+    settings = settings.allGhostSettings.edges[0].node
+
+    const author = getAuthorProperties(data)
+    const shareImage = author.image || _.get(settings, `cover_image`, null)
+    const title = `${data.name} - ${settings.title}`
+    const description = data.bio || config.siteDescriptionMeta || settings.description
+
+    return (
+        <>
+            <Helmet>
+                <title>{title}</title>
+                <meta name="description" content={description} />
+                <link rel="canonical" href={canonical} />
+                <meta property="og:site_name" content={settings.title} />
+                <meta property="og:type" content="profile" />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
+                <meta property="og:url" content={canonical} />
+                <meta name="twitter:title" content={title} />
+                <meta name="twitter:description" content={description} />
+                <meta name="twitter:url" content={canonical} />
+                {settings.twitter && <meta name="twitter:site" content={`https://twitter.com/${settings.twitter.replace(/^@/, ``)}/`} />}
+                {settings.twitter && <meta name="twitter:creator" content={settings.twitter} />}
+                <script type="application/ld+json">{`
+                    {
+                        "@context": "https://schema.org/",
+                        "@type": "Person",
+                        "name": "${data.name}",
+                        ${author.sameAsArray ? `"sameAs": ${author.sameAsArray},` : ``}
+                        "url": "${canonical}",
+                        ${shareImage ? `"image": {
+                                "@type": "ImageObject",
+                                "url": "${shareImage}",
+                                "width": "${config.shareImageWidth}",
+                                "height": "${config.shareImageHeight}"
+                            },` : ``}
+                        "mainEntityOfPage": {
+                            "@type": "WebPage",
+                            "@id": "${config.siteUrl}"
+                        },
+                        "description": "${description}"
+                    }
+                `}</script>
+            </Helmet>
+            <ImageMeta image={shareImage} />
+        </>
+    )
+}
+
+AuthorMeta.propTypes = {
+    data: PropTypes.shape({
+        name: PropTypes.string,
+        bio: PropTypes.string,
+        profile_image: PropTypes.string,
+        website: PropTypes.string,
+        twitter: PropTypes.string,
+        facebook: PropTypes.string,
+    }).isRequired,
+    settings: PropTypes.shape({
+        allGhostSettings: PropTypes.object.isRequired,
+    }).isRequired,
+    canonical: PropTypes.string.isRequired,
+}
+
+const AuthorMetaQuery = props => (
+    <StaticQuery
+        query={graphql`
+            query GhostSettingsAuthorMeta {
+                allGhostSettings {
+                    edges {
+                        node {
+                            ...GhostSettingsFields
+                        }
+                    }
+                }
+            }
+        `}
+        render={data => <AuthorMeta settings={data} {...props} />}
+    />
+)
+
+export default AuthorMetaQuery
