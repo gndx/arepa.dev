ac84196788590975ea37e7ecdd563290402d2af7
Oscar Barajas Tavares <oscar@arepa.dev>
2019-12-23 09:41:01 -0500

    [Update] config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,25 @@
-const path = require(`path`)
+const path = require('path');
 
-const config = require(`./src/utils/siteConfig`)
-const generateRSSFeed = require(`./src/utils/rss/generate-feed`)
+const config = require('./src/utils/siteConfig');
+const generateRSSFeed = require('./src/utils/rss/generate-feed');
 
-let ghostConfig
+let ghostConfig;
 
 try {
-    ghostConfig = require(`./.ghost`)
+  ghostConfig = require('./.ghost');
 } catch (e) {
-    ghostConfig = {
-        production: {
-            apiUrl: process.env.GHOST_API_URL,
-            contentApiKey: process.env.GHOST_CONTENT_API_KEY,
-        },
+  ghostConfig = {
+    production: {
+      apiUrl: process.env.GHOST_API_URL,
+      contentApiKey: process.env.GHOST_CONTENT_API_KEY
     }
+  };
 } finally {
-    const { apiUrl, contentApiKey } = process.env.NODE_ENV === `development` ? ghostConfig.development : ghostConfig.production
+  const { apiUrl, contentApiKey } = process.env.NODE_ENV === 'development' ? ghostConfig.development : ghostConfig.production;
 
-    if (!apiUrl || !contentApiKey || contentApiKey.match(/<key>/)) {
-        throw new Error(`GHOST_API_URL and GHOST_CONTENT_API_KEY are required to build. Check the README.`) // eslint-disable-line
-    }
+  if (!apiUrl || !contentApiKey || contentApiKey.match(/<key>/)) {
+    throw new Error(`GHOST_API_URL and GHOST_CONTENT_API_KEY are required to build. Check the README.`) // eslint-disable-line
+  }
 }
 
 /**
@@ -30,52 +30,69 @@ try {
 *
 */
 module.exports = {
-    siteMetadata: {
-        siteUrl: config.siteUrl,
-    },
-    plugins: [
-        /**
+  siteMetadata: {
+    siteUrl: config.siteUrl
+  },
+  plugins: [
+    /**
          *  Content Plugins
          */
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: path.join(__dirname, `src`, `pages`),
-                name: `pages`,
-            },
-        },
-        // Setup for optimised images.
-        // See https://www.gatsbyjs.org/packages/gatsby-image/
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: path.join(__dirname, `src`, `images`),
-                name: `images`,
-            },
-        },
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
-        {
-            resolve: `gatsby-source-ghost`,
-            options:
-                process.env.NODE_ENV === `development`
-                    ? ghostConfig.development
-                    : ghostConfig.production,
-        },
-        /**
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: path.join(__dirname, 'src', 'pages'),
+        name: 'pages'
+      }
+    },
+    // Setup for optimised images.
+    // See https://www.gatsbyjs.org/packages/gatsby-image/
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: path.join(__dirname, 'src', 'images'),
+        name: 'images'
+      }
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-ghost',
+      options:
+        process.env.NODE_ENV === 'development' ?
+          ghostConfig.development :
+          ghostConfig.production
+    },
+    /**
          *  Utility Plugins
          */
-        {
-            resolve: `gatsby-plugin-ghost-manifest`,
-            options: {
-                short_name: config.shortTitle,
-                start_url: `/`,
-                background_color: config.backgroundColor,
-                theme_color: config.themeColor,
-                display: `minimal-ui`,
-                icon: `static/${config.siteIcon}`,
-                legacy: true,
-                query: `
+    {
+      resolve: 'gatsby-plugin-ghost-manifest',
+      options: {
+        short_name: config.shortTitle,
+        start_url: '/',
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
+        display: 'minimal-ui',
+        icon: `static/${config.siteIcon}`,
+        legacy: true,
+        query: `
+                {
+                    allGhostSettings {
+                        edges {
+                            node {
+                                title
+                                description
+                            }
+                        }
+                    }
+                }
+              `
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
                 {
                     allGhostSettings {
                         edges {
@@ -87,32 +104,15 @@ module.exports = {
                     }
                 }
               `,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-feed`,
-            options: {
-                query: `
-                {
-                    allGhostSettings {
-                        edges {
-                            node {
-                                title
-                                description
-                            }
-                        }
-                    }
-                }
-              `,
-                feeds: [
-                    generateRSSFeed(config),
-                ],
-            },
-        },
-        {
-            resolve: `gatsby-plugin-advanced-sitemap`,
-            options: {
-                query: `
+        feeds: [
+          generateRSSFeed(config)
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-advanced-sitemap',
+      options: {
+        query: `
                 {
                     allGhostPost {
                         edges {
@@ -155,33 +155,33 @@ module.exports = {
                         }
                     }
                 }`,
-                mapping: {
-                    allGhostPost: {
-                        sitemap: `posts`,
-                    },
-                    allGhostTag: {
-                        sitemap: `tags`,
-                    },
-                    allGhostAuthor: {
-                        sitemap: `authors`,
-                    },
-                    allGhostPage: {
-                        sitemap: `pages`,
-                    },
-                },
-                exclude: [
-                    `/dev-404-page`,
-                    `/404`,
-                    `/404.html`,
-                    `/offline-plugin-app-shell-fallback`,
-                ],
-                createLinkInHead: true,
-                addUncaughtPages: true,
-            },
+        mapping: {
+          allGhostPost: {
+            sitemap: 'posts'
+          },
+          allGhostTag: {
+            sitemap: 'tags'
+          },
+          allGhostAuthor: {
+            sitemap: 'authors'
+          },
+          allGhostPage: {
+            sitemap: 'pages'
+          }
         },
-        `gatsby-plugin-catch-links`,
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-force-trailing-slashes`,
-        `gatsby-plugin-offline`,
-    ],
-}
+        exclude: [
+          '/dev-404-page',
+          '/404',
+          '/404.html',
+          '/offline-plugin-app-shell-fallback'
+        ],
+        createLinkInHead: true,
+        addUncaughtPages: true
+      }
+    },
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-force-trailing-slashes',
+    'gatsby-plugin-offline'
+  ]
+};
--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'gatsby';
 
 /**
 * Navigation component
@@ -13,29 +13,28 @@ import { Link } from 'gatsby'
 *
 */
 const Navigation = ({ data, navClass }) => (
-    <>
-        {data.map((navItem, i) => {
-            if (navItem.url.match(/^\s?http(s?)/gi)) {
-                return <a className={navClass} href={navItem.url} key={i} target="_blank" rel="noopener noreferrer">{navItem.label}</a>
-            } else {
-                return <Link className={navClass} to={navItem.url} key={i}>{navItem.label}</Link>
-            }
-        })}
-    </>
-)
+  <>
+    {data.map((navItem, i) => {
+      if (navItem.url.match(/^\s?http(s?)/gi)) {
+        return <a className={navClass} href={navItem.url} key={i} target="_blank" rel="noopener noreferrer">{navItem.label}</a>;
+      }
+      return <Link className={navClass} to={navItem.url} key={i}>{navItem.label}</Link>;
+    })}
+  </>
+);
 
 Navigation.defaultProps = {
-    navClass: `site-nav-item`,
-}
+  navClass: 'site-nav-item'
+};
 
 Navigation.propTypes = {
-    data: PropTypes.arrayOf(
-        PropTypes.shape({
-            label: PropTypes.string.isRequired,
-            url: PropTypes.string.isRequired,
-        }).isRequired,
-    ).isRequired,
-    navClass: PropTypes.string,
-}
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired
+    }).isRequired,
+  ).isRequired,
+  navClass: PropTypes.string
+};
 
-export default Navigation
+export default Navigation;
