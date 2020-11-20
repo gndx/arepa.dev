b43cb9dcd37cc6adf61ebd80d8c2970367a1c65b
Oscar Barajas <oscar@arepa.dev>
2020-11-20 10:11:44 -0500

    [ADD] minimal changes

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteUrl: 'https://arepa.dev',
-  postsPerPage: 8,
+  postsPerPage: 16,
   siteTitleMeta: 'Arepa.dev',
   siteDescriptionMeta: 'Recursos en español para aprender JavaScript',
   shareImageWidth: 1000,
   shareImageHeight: 523,
   shortTitle: 'Arepa.dev',
-  siteIcon: 'favicon.png',
+  siteIcon: '1.png',
   backgroundColor: '#e9e9e9',
   themeColor: '#15171A'
 };
